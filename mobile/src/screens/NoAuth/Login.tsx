66e4d8e75c95212ade6e2f85b2edc90340a51a88
GabrielCarvalhoDeLima <gabriel.carvalho@redecredauto.com.br>
2025-03-25 15:57:21 -0300

    first commit 🚀

--- mobile/src/screens/NoAuth/Login.tsx
+++ mobile/src/screens/NoAuth/Login.tsx
@@ -11,10 +11,9 @@ import { zodResolver } from "@hookform/resolvers/zod";
 import { NoAuthNavigatorRoutesProps } from '@routes/app.no.auth.routes';
 
 const loginSchema = z.object({
-  email: z.string().nonempty("Informe o email").email("Email inválido"),
+  email: z.string().min(5, { message: "Must be 5 or more characters long" }),
   password: z.string().nonempty("Informe a senha").min(6, "A senha precisa ter pelo menos 6 dígitos"),
 });
-
 
 type FormDataProps = z.infer<typeof loginSchema>;
 
@@ -24,7 +23,7 @@ export default function Login() {
 
   const { control, handleSubmit, formState: { errors } } = useForm<FormDataProps>({
     resolver: zodResolver(loginSchema)
-  })
+  });
 
   async function handleLogin(data: FormDataProps) {
     try {
@@ -41,50 +40,50 @@ export default function Login() {
     navigation.navigate("register");
   }
 
-
   return (
-    <ScrollView className="bg-white flex-1" showsVerticalScrollIndicator={false}>
-      <View className="bg-base-gray-6 items-center rounded-b-[30px]">
-        <View className="items-center pt-40">
-          <Logo className="w-full h-96" />
-          <LogoText className="w-full h-96" />
-          <Text className="text-sm font-light font-karla text-base-gray-3 leading-base">Seu espaço de compra e venda</Text>
-        </View>
+    <View className="flex-1 bg-white">
+      <ScrollView className="flex-grow" contentContainerStyle={{ flexGrow: 1 }}>
+        <View className="bg-base-gray-6 items-center rounded-b-[30px] pb-14">
+          <View className="items-center pt-40">
+            <Logo className="w-full h-96" />
+            <LogoText className="w-full h-96" />
+            <Text className="text-sm font-light font-karla text-base-gray-3 leading-base">Seu espaço de compra e venda</Text>
+          </View>
 
-        <View className="pt-20 pb-16 w-80">
-          <Text className="mb-4 text-center">Acesse sua conta</Text>
+          <View className="pt-20 w-80">
+            <Text className="mb-4 text-center">Acesse sua conta</Text>
 
-          <Controller
-            control={control}
-            name="email"
-            render={({ field: { onChange, value } }) => (
-              <Input keyboardType='email-address' value={value} onChangeText={onChange} placeholder="E-mail" errorMessage={errors.email?.message} />
-            )}
-          />
-
-          <View className="mt-5">
             <Controller
               control={control}
-              name="password"
+              name="email"
               render={({ field: { onChange, value } }) => (
-                <Input value={value} onChangeText={onChange} isPasswordInput placeholder="Senha" errorMessage={errors.password?.message} />
+                <Input keyboardType='email-address' value={value} onChangeText={onChange} placeholder="E-mail" errorMessage={errors.email?.message} />
               )}
             />
+
+            <View className="mt-5">
+              <Controller
+                control={control}
+                name="password"
+                render={({ field: { onChange, value } }) => (
+                  <Input value={value} onChangeText={onChange} isPasswordInput placeholder="Senha" errorMessage={errors.password?.message} />
+                )}
+              />
+            </View>
+
+            <View className="mt-5">
+              <Button onPress={handleSubmit(handleLogin)} isLoading={isLoading} color="primary" title="Entrar" />
+            </View>
           </View>
+        </View>
 
-          <View className="mt-5">
-            <Button onPress={handleSubmit(handleLogin)} isLoading={isLoading} color="primary" title="Entrar" />
+        <View className="flex-1 justify-end items-center pb-10">
+          <Text className="text-base-gray-2 font-light leading-base">Ainda não tem acesso?</Text>
+          <View className="w-80 mt-4">
+            <Button onPress={handleRegister} color="gray" title="Criar conta" />
           </View>
-
         </View>
-      </View>
-
-      <View className="items-center pt-20">
-        <Text className="text-base-gray-2 font-light leading-base">Ainda não tem acesso?</Text>
-        <View className="w-80 mt-8">
-          <Button onPress={handleRegister} color="gray" title="Criar conta" />
-        </View>
-      </View>
-    </ScrollView>
-  )
+      </ScrollView>
+    </View>
+  );
 }
