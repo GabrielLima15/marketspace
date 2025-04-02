4bd1fc5c86643a1cd156342b54ef39f63d20f5fc
GabrielCarvalhoDeLima <gabriel.carvalho@redecredauto.com.br>
2025-04-02 15:44:28 -0300

    fix: pegando avatar do usuario logado

--- mobile/src/components/Avatar.tsx
+++ mobile/src/components/Avatar.tsx
@@ -1,10 +1,19 @@
+import { useAuth } from "@hooks/useAuth";
+import { api } from "@services/api";
 import { User } from "phosphor-react-native";
-import { View } from "react-native";
+import { Image, View } from "react-native";
 
 export default function Avatar() {
+
+  const { user } = useAuth()
+
   return (
-    <View className="border-2 border-product-blue-light rounded-full p-2">
-      <User size={32} />
+    <View className="border-2 border-product-blue-light rounded-full">
+      {user?.avatar ? <Image
+        className="w-10 h-10 rounded-full"
+        source={{ uri: `${api.defaults.baseURL}/images/${user.avatar}` }}
+        onError={(e) => console.log("Erro ao carregar imagem:", e.nativeEvent)}
+      /> : <User size={32} />}
     </View>
   )
 }
