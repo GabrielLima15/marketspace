9d8e847d3f56aa5ea0eea101617e240b8c8d32c0
GabrielLima15 <gabrielcarvalho850@outlook.com>
2025-05-14 11:25:59 -0300

    feat: criar DTO para o produto com informações detalhadas

---
+++ mobile/src/dtos/ProductDTO.ts
@@ -0,0 +1,24 @@
+export type ProductDTO = {
+  id: string;
+  name: string;
+  description: string;
+  is_new: boolean;
+  price: number;
+  accept_trade: boolean;
+  user_id: string;
+  is_active: boolean;
+  created_at: string;
+  updated_at: string;
+  product_images: Productimage[];
+  payment_methods: Paymentmethod[];
+}
+
+interface Paymentmethod {
+  key: string;
+  name: string;
+}
+
+interface Productimage {
+  path: string;
+  id: string;
+}
