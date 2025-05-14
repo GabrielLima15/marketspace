e57f0837fa4c4214ba59cadc21666e2091c7ef70
GabrielLima15 <gabrielcarvalho850@outlook.com>
2025-05-14 15:39:44 -0300

    refactor: atualizar o componente MyAdsSummaryCard para utilizar o hook useProduct e exibir a quantidade de produtos ativos

--- mobile/src/components/MyAdsSummaryCard.tsx
+++ mobile/src/components/MyAdsSummaryCard.tsx
@@ -1,26 +1,25 @@
+import { useProduct } from "@hooks/useProduct";
 import { useNavigation } from "@react-navigation/native";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
 import { AppAuthBottomTabRoutes } from "@routes/app.auth.routes";
 import { ArrowRight, Tag } from "phosphor-react-native";
 import { Text, TouchableOpacity, View } from "react-native";
 
-type Props = {
-  numberOfAds?: number;
-}
-
-export default function MyAdsSummaryCard({ numberOfAds }: Props) {
+export default function MyAdsSummaryCard() {
 
   const navigation = useNavigation<NativeStackNavigationProp<AppAuthBottomTabRoutes>>();
 
   const handleNavigateToMyAds = () => navigation.navigate("myads");
 
+  const { products } = useProduct();
+
   return (
-    numberOfAds ? (
+    products.length > 0 ? (
       <View className="w-full flex-row items-center justify-between h-20 px-4 mt-4 bg-[rgba(100,121,199,0.3)] rounded-md">
         <View className="flex-row items-center gap-3">
           <Tag size={20} color="#364D9D" weight="regular" />
           <View>
-            <Text className="font-bold text-base text-base-gray-2">{numberOfAds}</Text>
+            <Text className="font-bold text-base text-base-gray-2">{products.length}</Text>
             <Text className="text-sm text-base-gray-2">anúncios ativos</Text>
           </View>
         </View>
