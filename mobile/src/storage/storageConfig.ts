18c3e7a865b51016b7d3b97852d8a6a0cdcbe026
GabrielLima15 <gabrielcarvalho850@outlook.com>
2025-05-14 11:26:11 -0300

    feat: atualizar configurações de armazenamento para o novo namespace do MarketSpace

--- mobile/src/storage/storageConfig.ts
+++ mobile/src/storage/storageConfig.ts
@@ -1,4 +1,5 @@
-const USER_STORAGE = '@ignitegym:user'
-const AUTH_TOKEN_STORAGE = '@ignitegym:token'
+const USER_STORAGE = '@marketspace:user'
+const AUTH_TOKEN_STORAGE = '@marketspace:token'
+const PRODUCTS_STORAGE = '@marketspace:products'
 
-export { USER_STORAGE, AUTH_TOKEN_STORAGE }
+export { USER_STORAGE, AUTH_TOKEN_STORAGE, PRODUCTS_STORAGE }
