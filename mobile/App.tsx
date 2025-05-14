a6cca94209e54c8e4533daba10c03c6c73098325
GabrielLima15 <gabrielcarvalho850@outlook.com>
2025-05-14 11:44:03 -0300

    refactor: substituir AuthContextProvider por AppContext no componente App

--- mobile/App.tsx
+++ mobile/App.tsx
@@ -1,23 +1,22 @@
 import { StatusBar, View } from 'react-native';
 import { useFonts, Karla_400Regular, Karla_700Bold } from '@expo-google-fonts/karla';
 import './global.css';
-import { AuthContextProvider } from '@contexts/AuthContext';
 import AppRoutes from '@routes/app.routes';
 import { GestureHandlerRootView } from 'react-native-gesture-handler';
 import 'react-native-reanimated';
+import AppContext from '@contexts/AppContext';
 
 
 export default function App() {
 
   const [fontsLoaded] = useFonts({ Karla_400Regular, Karla_700Bold })
-  console.log("🚀 ~ App ~ fontsLoaded:", fontsLoaded)
 
   return (
     <GestureHandlerRootView className='flex-1'>
-      <AuthContextProvider>
+      <AppContext>
         <StatusBar barStyle={'dark-content'} backgroundColor="transparent" translucent />
         {fontsLoaded ? <AppRoutes /> : <View />}
-      </AuthContextProvider>
+      </AppContext>
     </GestureHandlerRootView>
   );
 }
