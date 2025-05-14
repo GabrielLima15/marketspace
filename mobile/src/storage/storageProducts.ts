8521c97dcf8d9f63474c048d772c532fc3a090fc
GabrielLima15 <gabrielcarvalho850@outlook.com>
2025-05-14 11:40:19 -0300

    feat: renomear e ajustar funções de armazenamento para produtos no AsyncStorage

--- mobile/src/storage/storageProducts.ts
+++ mobile/src/storage/storageProducts.ts
@@ -1,20 +1,18 @@
+// storageProducts.ts
 import AsyncStorage from "@react-native-async-storage/async-storage";
-
 import { PRODUCTS_STORAGE } from "./storageConfig";
 import { ProductDTO } from "@dtos/ProductDTO";
 
-export async function storageUserSave(user: ProductDTO) {
-  await AsyncStorage.setItem(PRODUCTS_STORAGE, JSON.stringify(user))
+export async function storageProductsSave(products: ProductDTO[]) {
+  await AsyncStorage.setItem(PRODUCTS_STORAGE, JSON.stringify(products));
 }
 
-export async function storageUserGet() {
+export async function storageProductsGet(): Promise<ProductDTO[]> {
   const storage = await AsyncStorage.getItem(PRODUCTS_STORAGE);
-
-  const products: ProductDTO = storage ? JSON.parse(storage) : {}
-
+  const products: ProductDTO[] = storage ? JSON.parse(storage) : [];
   return products;
 }
 
-export async function storageUserRemove() {
+export async function storageProductsRemove() {
   await AsyncStorage.removeItem(PRODUCTS_STORAGE);
 }
