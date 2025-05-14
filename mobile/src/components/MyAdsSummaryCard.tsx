41ad1e65ddd323c112054dace836f69faf0eac72
GabrielLima15 <gabrielcarvalho850@outlook.com>
2025-05-14 15:53:38 -0300

    refactor: atualizar o componente MyAdsSummaryCard para exibir produtos do usuário e melhorar a apresentação dos anúncios ativos

--- mobile/src/components/MyAdsSummaryCard.tsx
+++ mobile/src/components/MyAdsSummaryCard.tsx
@@ -11,26 +11,33 @@ export default function MyAdsSummaryCard() {
 
   const handleNavigateToMyAds = () => navigation.navigate("myads");
 
-  const { products } = useProduct();
+  const { userProducts } = useProduct();
 
   return (
-    products.length > 0 ? (
-      <View className="w-full flex-row items-center justify-between h-20 px-4 mt-4 bg-[rgba(100,121,199,0.3)] rounded-md">
-        <View className="flex-row items-center gap-3">
-          <Tag size={20} color="#364D9D" weight="regular" />
-          <View>
-            <Text className="font-bold text-base text-base-gray-2">{products.length}</Text>
-            <Text className="text-sm text-base-gray-2">anúncios ativos</Text>
-          </View>
-        </View>
+    userProducts.length > 0 ? (
+      <>
+        <Text className="pt-8 text-base text-base-gray-3 font-karla font-normal leading-base">
+          Seus produtos anunciados para venda
+        </Text>
 
-        <TouchableOpacity onPress={handleNavigateToMyAds} className="flex-row items-center gap-2">
-          <Text className="text-sm text-product-blue-light font-bold">
-            Meus anúncios
-          </Text>
-          <ArrowRight size={16} color="#364D9D" />
-        </TouchableOpacity>
-      </View>
+        <View className="w-full flex-row items-center justify-between h-20 px-4 mt-4 bg-[rgba(100,121,199,0.3)] rounded-md">
+          <View className="flex-row items-center gap-3">
+            <Tag size={20} color="#364D9D" weight="regular" />
+            <View>
+              <Text className="font-bold text-base text-base-gray-2">{userProducts.length}</Text>
+              <Text className="text-sm text-base-gray-2">anúncios ativos</Text>
+            </View>
+          </View>
+
+          <TouchableOpacity onPress={handleNavigateToMyAds} className="flex-row items-center gap-2">
+            <Text className="text-sm text-product-blue-light font-bold">
+              Meus anúncios
+            </Text>
+            <ArrowRight size={16} color="#364D9D" />
+          </TouchableOpacity>
+        </View>
+      </>
+
     ) : null
   );
 }
