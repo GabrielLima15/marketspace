9ec548fd049124ea23e0349022f48568a1c717cf
GabrielCarvalhoDeLima <gabriel.carvalho@redecredauto.com.br>
2025-05-21 16:38:14 -0300

    refactor: modificar a função de criação de usuários para utilizar FormData, melhorando o envio de dados para a API

--- mobile/src/services/users.ts
+++ mobile/src/services/users.ts
@@ -1,7 +1,13 @@
 import { GET, POST } from "./api";
 
 export const Create = async (avatar: string, name: string, email: string, tel: string, password: string) => {
-    const response = await POST('/users', { avatar, name, email, tel, password })
+    const formData = new FormData();
+    formData.append('avatar', avatar);
+    formData.append('name', name);
+    formData.append('email', email);
+    formData.append('tel', tel);
+    formData.append('password', password);
+    const response = await POST('/users', formData, true)
     return response.data
 }
 
