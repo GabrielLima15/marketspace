3bb2825e3de722d0a1dd5a5b213fbda9e1b7197b
GabrielLima15 <gabrielcarvalho850@outlook.com>
2025-05-22 11:29:41 -0300

    refactor: modificar funções de Login e RefreshToken para retornar dados em formato de objeto, melhorando a estrutura da resposta

--- mobile/src/services/sessions.ts
+++ mobile/src/services/sessions.ts
@@ -2,9 +2,10 @@ import { POST } from "./api";
 
 export const Login = async (email: string, password: string) => {
     const response = await POST('/sessions', { email, password })
-    return response.data
+    return { data: response.data }
 }
+
 export const RefreshToken = async (refresh_token: string) => {
     const response = await POST('/sessions/refresh-token', { refresh_token })
-    return response.data
+    return { data: response.data }
 }
