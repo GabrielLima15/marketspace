1855e516a8f31cbd0773ffccc6510fca4ecb6e4e
GabrielCarvalhoDeLima <gabriel.carvalho@redecredauto.com.br>
2025-05-22 10:40:34 -0300

    refactor: atualizar importação do hook useAuth para utilizar o contexto AuthContext na rota principal do aplicativo

--- mobile/src/routes/app.routes.tsx
+++ mobile/src/routes/app.routes.tsx
@@ -1,9 +1,9 @@
-import { useAuth } from '@hooks/useAuth'
 import { NavigationContainer } from '@react-navigation/native'
 import { NoAuthRoutes } from './app.no.auth.routes'
 import { AuthRoutes } from './app.auth.routes'
 import Loading from '@components/Loading'
 import Toast from 'react-native-toast-message'
+import { useAuth } from '@contexts/AuthContext'
 
 export default function AppRoutes() {
 
