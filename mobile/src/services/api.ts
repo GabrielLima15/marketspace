db1758fd6b4a629e3f71f6d717c604eb3326201d
GabrielCarvalhoDeLima <gabriel.carvalho@redecredauto.com.br>
2025-05-22 10:40:51 -0300

    refactor: atualizar URL base da API para ambiente de emulação

--- mobile/src/services/api.ts
+++ mobile/src/services/api.ts
@@ -14,7 +14,7 @@ type APIInstanceProps = AxiosInstance & {
 }
 
 const api = axios.create({
-  baseURL: 'http://192.168.0.38:3333'
+  baseURL: 'http://10.0.2.2:3333'
 }) as APIInstanceProps
 
 let failedQueue: Array<PromiseType> = []
