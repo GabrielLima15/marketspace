8c6323b0bfad65ed5a9426cae7cc6aa04b01bbcc
GabrielLima15 <gabrielcarvalho850@outlook.com>
2025-05-22 11:33:42 -0300

    refactor: otimizar a renderização do componente MyAdsSummaryCard para retornar null quando não houver produtos do usuário

--- mobile/src/components/MyAdsSummaryCard.tsx
+++ mobile/src/components/MyAdsSummaryCard.tsx
@@ -7,38 +7,36 @@ import React from "react";
 import { Text, TouchableOpacity, View } from "react-native";
 
 export default function MyAdsSummaryCard() {
-
   const navigation = useNavigation<NativeStackNavigationProp<AppAuthBottomTabRoutes>>();
-
   const handleNavigateToMyAds = () => navigation.navigate("myads");
-
   const { userProducts } = useProduct();
 
+  if (!userProducts || userProducts.length === 0) {
+    return null;
+  }
+
   return (
-    userProducts.length > 0 ? (
-      <>
-        <Text className="pt-8 text-base text-base-gray-3 font-karla font-normal leading-base">
-          Seus produtos anunciados para venda
-        </Text>
+    <>
+      <Text className="pt-8 text-base text-base-gray-3 font-karla font-normal leading-base">
+        Seus produtos anunciados para venda
+      </Text>
 
-        <View className="w-full flex-row items-center justify-between h-20 px-4 mt-4 bg-[rgba(100,121,199,0.3)] rounded-md">
-          <View className="flex-row items-center gap-3">
-            <Tag size={20} color="#364D9D" weight="regular" />
-            <View>
-              <Text className="font-bold text-base text-base-gray-2">{userProducts.length}</Text>
-              <Text className="text-sm text-base-gray-2">anúncios ativos</Text>
-            </View>
+      <View className="w-full flex-row items-center justify-between h-20 px-4 mt-4 bg-[rgba(100,121,199,0.3)] rounded-md">
+        <View className="flex-row items-center gap-3">
+          <Tag size={20} color="#364D9D" weight="regular" />
+          <View>
+            <Text className="font-bold text-base text-base-gray-2">{userProducts.length}</Text>
+            <Text className="text-sm text-base-gray-2">anúncios ativos</Text>
           </View>
-
-          <TouchableOpacity onPress={handleNavigateToMyAds} className="flex-row items-center gap-2">
-            <Text className="text-sm text-product-blue-light font-bold">
-              Meus anúncios
-            </Text>
-            <ArrowRight size={16} color="#364D9D" />
-          </TouchableOpacity>
         </View>
-      </>
 
-    ) : null
+        <TouchableOpacity onPress={handleNavigateToMyAds} className="flex-row items-center gap-2">
+          <Text className="text-sm text-product-blue-light font-bold">
+            Meus anúncios
+          </Text>
+          <ArrowRight size={16} color="#364D9D" />
+        </TouchableOpacity>
+      </View>
+    </>
   );
 }
