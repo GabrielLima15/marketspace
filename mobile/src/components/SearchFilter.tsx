b5892ac2cbdbee2743225caee34093afb3aa71c6
GabrielLima15 <gabrielcarvalho850@outlook.com>
2025-05-26 11:33:09 -0300

    refactor: adicionar funcionalidades de busca e filtro ao componente SearchFilter, incluindo novos métodos e melhorias na interação do usuário

--- mobile/src/components/SearchFilter.tsx
+++ mobile/src/components/SearchFilter.tsx
@@ -1,22 +1,30 @@
 import { MagnifyingGlass } from "phosphor-react-native";
-import { TextInput, TextInputProps, View } from "react-native";
+import { TextInput, TextInputProps, TouchableOpacity, View } from "react-native";
 import FilterModal from "./FilterModal";
 
 type Props = TextInputProps & {
-
+  onSearch: (query: string) => void;
+  onFilter: (filters: {
+    is_new?: boolean;
+    accept_trade?: boolean;
+    payment_methods: string[];
+  }) => void;
 }
 
-export default function SearchFilter({ ...rest }: Props) {
+export default function SearchFilter({ onSearch, onFilter, ...rest }: Props) {
   return (
     <View className="bg-white w-full rounded-md mt-4 flex-row items-center justify-between px-4 py-2">
       <TextInput
-        className=""
+        className="flex-1"
+        onChangeText={onSearch}
         {...rest}
       />
 
       <View className="flex-row items-center gap-2">
-        <MagnifyingGlass size={20} weight="bold" />
-        <FilterModal />
+        <TouchableOpacity onPress={() => onSearch(rest.value || '')}>
+          <MagnifyingGlass size={20} weight="bold" />
+        </TouchableOpacity>
+        <FilterModal onApplyFilters={onFilter} />
       </View>
     </View>
   )
